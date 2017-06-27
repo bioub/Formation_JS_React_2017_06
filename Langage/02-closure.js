535642ac5991d894c36f888d56170d4fb580cd3c
stage <stage@localhost.localdomain>
2017-06-27 16:13:12 +0200

    Correction Ex JS

---
+++ Langage/02-closure.js
@@ -0,0 +1,40 @@
+
+const logClosure = function(msg) {
+  // Portée de closure (portée sauvegardée automatiquement)
+  // 2 conditions :
+  // 1 - 2 fonctions imbriquées
+  // 2 - la fonction interne puisse être appelée en dehors
+  return function() {
+    console.log(msg);
+  };
+};
+
+const logHello = logClosure('Hello');
+
+logHello();
+logHello();
+logHello();
+
+/*
+const createButton = function(value) {
+  let button = document.createElement('button');
+  button.innerHTML = value;
+  button.addEventListener('click', function() {
+    console.log(value);
+  });
+  return button;
+}
+
+const monBouton = createButton('Clic moi');
+ */
+
+for (var i=0; i<3; i++) {
+  setTimeout(function onTimeout() {
+    console.log(i);
+  }, 1000)
+}
+
+for (var i=0; i<3; i++) {
+  setTimeout(logClosure(i), 1000)
+}
+
